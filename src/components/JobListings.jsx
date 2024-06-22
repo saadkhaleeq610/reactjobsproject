acbc8928d1f3e9f6c980ebe34e48ef739cee37b6
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-06-22 11:05:48 -0700

    update

---
+++ src/components/JobListings.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import JobListing from './JobListing';
+import Spinner from './Spinner';
+import jobs from '../jobs.json'
+
+const JobListings = ({ isHome = false }) => {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <section className='bg-blue-50 px-4 py-10'>
+      <div className='container-xl lg:container m-auto'>
+        <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
+          {isHome ? 'Recent Jobs' : 'Browse Jobs'}
+        </h2>
+
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+export default JobListings;
